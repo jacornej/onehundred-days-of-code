cbf747e25d01a38ab712fc18aec59c432c920ab8
jacornej <jasmincornejo107@gmail.com>
2023-11-01 20:12:21 -0700

    add basic python requirments

---
+++ day-1/hello_world.py
@@ -0,0 +1,15 @@
+"""
+docustring
+"""
+
+
+def main():
+    """
+    This program prints hello of names.
+    """
+    name = input("what is your names?")
+    print(f"Hello, {name}")
+
+
+if __name__ == "__main__":
+    main()
