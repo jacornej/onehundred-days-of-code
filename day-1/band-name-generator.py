e9befab323950a20a1422ba8d7c0eed16b22753b
jacornej <jasmincornejo107@gmail.com>
2023-11-01 20:14:43 -0700

    Create Band Name Generator

---
+++ day-1/band-name-generator.py
@@ -0,0 +1,11 @@
+# create a greeting for your program
+print("Hello Welcome to the Band Name Generator")
+
+# Ask the user for the city that they grew up in
+city = input("Input the name of the city you grew up in: \n")
+# Ask the user for the name of a pet
+pet = input("Input the name of a pet: \n")
+
+# combine the name of their city and pet and show them their band name
+# Make sure the input cursor shows on a new line:
+print("Your band name is: " + city + pet )
